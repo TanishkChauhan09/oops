b4ccb51d3a81be888a1cc3b61f091d864877b64c
TANISHK CHAUHAN <iamtanishkchauhan@gmail.com>
2026-05-10 00:56:01 +0530

    Need for class and object and cpp earlier called c with classes

--- first.cpp
+++ first.cpp
@@ -1,14 +1,38 @@
 #include<iostream>
 using namespace std;
 
-int main()
-{
+class Student{
+
+    public:
     string name;
     int age;
     int roll_number;
     string grade;
 
-    cin>>name>>age>>roll_number>>grade;
+};
 
-    cout<<name<<" "<<age<<" "<<roll_number<<" "<<grade<<endl;
+int main()
+{
+    // string name;
+    // int age;
+    // int roll_number;
+    // string grade;
+
+    // cin>>name>>age>>roll_number>>grade;
+
+    // cout<<name<<" "<<age<<" "<<roll_number<<" "<<grade<<endl;
+
+    Student s1;
+    s1.name="John";
+    s1.age=20;
+    s1.roll_number=101;
+    s1.grade="A";
+    cout<<s1.name<<" "<<s1.age<<" "<<s1.roll_number<<" "<<s1.grade<<endl;
+
+    Student s2;
+    s2.name="lily";
+    s2.age=22;
+    s2.roll_number=102;
+    s2.grade="A";
+    cout<<s2.name<<" "<<s2.age<<" "<<s2.roll_number<<" "<<s2.grade<<endl;
 }
