3af5e24038b70966d7a0e4e4780c23bb6091fc9d
TANISHK CHAUHAN <iamtanishkchauhan@gmail.com>
2026-05-17 15:00:08 +0530

    Destructor in oops

---
+++ first7.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student{
+
+    private:
+    string name;
+    int *age;
+
+    public:
+    Student(string name,int age)
+    {
+        this->name = name;
+        this->age = new int(age);
+        cout<<"Constructor called for "<<name<<endl;
+    }
+
+    ~Student(){
+        delete age;
+        cout<<"Destructor called for "<<name<<endl;
+    }
+};
+
+int main()
+{
+    Student s("Rohan",20);
+    Student s2("Sohan",21);
+}
