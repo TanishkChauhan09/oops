675d6edc041c8556839df2873880010795602c4f
TANISHK CHAUHAN <iamtanishkchauhan@gmail.com>
2026-06-02 19:07:26 +0530

    Single inheritance

---
+++ first12.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Human{
+
+    protected:
+     string name;
+     int age;
+
+     public:
+    //  Human(string name,int age){
+    //     this->name = name;
+    //     this->age = age;
+    //  }
+
+    Human(){}
+
+     void mtd_parent(){
+        cout<<"This is parent class method"<<endl;
+     }
+};
+
+class Student:public Human{
+
+    private:
+    string grade;
+    int fees;
+    string school;
+
+    public:
+  
+    //  ye kr skte hai jis se ke phle parent class ka constructor call hoga,pr un variables ke liye ek dedicatedly constructor bnana pdega parent class me else default constructor bnana pdega agar ye use nhi kr rha hoon means child class ke constructor me hi saare variables ko initialize kr rha hoon toh parent class ke constructor ki jarurat nhi hai bss default constructor ki jrurat h
+
+    // Student(string name,int age,string grade,int fees,string school):Human(name,age){
+    //     this->grade = grade;
+    //     this->fees = fees;
+    //     this->school = school;
+    // }
+
+    // ye bhi kr skte hai
+    Student(string name,int age,string grade,int fees,string school){
+        this->name = name;
+        this->age = age;
+        this->grade = grade;
+        this->fees = fees;
+        this->school = school;
+
+    }
+
+    void display(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"Age: "<<age<<endl;
+        cout<<"Grade: "<<grade<<endl;
+        cout<<"Fees: "<<fees<<endl;
+        cout<<"School: "<<school<<endl;
+
+        cout<<"______________________________________________________"<<endl;
+    }
+       
+};
+
+int main(){
+
+    Student s1("Rohan",20,"A",5000,"ABC School");
+    s1.display();
+    s1.mtd_parent();
+
+    return 0;
+
+}
