afb985bf88952b98c4e8dff484c9f2e388aa1eb8
TANISHK CHAUHAN <iamtanishkchauhan@gmail.com>
2026-06-08 18:48:55 +0530

    Exception handling(runtime error)

---
+++ first25.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    try{ 
+        int *arr = new int[1000000000000000000];
+        cout<<"Memory Alllocated"<<endl;
+        delete []arr ;
+    }
+
+    catch(const exception &e) // exception is a class , try jisko error throw kregi wo yhi class hai , ab wo(exception) class object dega catch me jisko maine ek 'e' naam ka variable jo exception type ka hai usme le liya hai
+    {
+        cout<<"Error occured: "<<e.what()<<endl;  // e object hone se exception class ke method ko call krskta hoon toh what() whi method hai jisme by default kuch return ho rha hoga 
+        return 0;
+    }
+}
+
+// Output is: 
+//            Exception Occured due to line 7 : std :: bad_alloc
