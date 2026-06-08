55d12002e1b04ac7e901c023636c93328daf4a12
TANISHK CHAUHAN <iamtanishkchauhan@gmail.com>
2026-06-08 19:15:50 +0530

    Exception handling(runtime error)

--- first25.cpp
+++ first25.cpp
@@ -21,5 +21,5 @@ int main(){
 //            Exception Occured due to line 7 : std :: bad_alloc
 
 
-// i can also write : throw runtime_error("Divide is not possible as division by 0 is not possible"); in this case in catch i have to write because runtime_error class will return object so : catch(const exception &e){cout<<"Error:"<<e.what()<<endl} like this
+// i can also write : throw runtime_error("Divide is not possible as division by 0 is not possible"); in this case in catch i have to write because runtime_error class will return object so : catch(const exception &e){cout<<"Error:"<<e.what()<<endl} like this, and parent pointer can handle child class oject so runtime_error class ka object , parent(exception) class ka object handle kr lega
 //       as well as, : throw "Divide is not possible as division by 0 is not possible";       
