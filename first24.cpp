49db7251a2dd44ca8a98551d1620054d9c6714d4
TANISHK CHAUHAN <iamtanishkchauhan@gmail.com>
2026-06-08 18:41:15 +0530

    Exception handling (runtime error)

--- first24.cpp
+++ first24.cpp
@@ -7,22 +7,40 @@ int main(){
     cout<<"Enter two numbers: ";
     cin>>a>>b;
 
-    if(b==0)
+    // if(b==0)
+    // {
+    //     cout<<"Divide is not possible as divide by 0 is possible"<<endl;
+    //     return 0;
+    // }
+
+    // int res = a/b;
+
+    // cout<<"Division of a and b is: "<<res<<endl;
+    // return 0;
+
+    // Output is :
+    //             Enter two numbers: 5 0
+    //             Divide is not possible as divide by 0 is possible
+
+    //             Enter two numbers: 6 2
+    //             Division of a and b is: 3
+
+    try{
+        if(b==0)
+        throw  "Divide is not possible as denominator is 0";
+
+        int res = a/b;
+        cout<<"Division of a and b is: "<<res<<endl;
+    }
+    catch(const char *e) 
     {
-        cout<<"Divide is not possible as divide by 0 is possible"<<endl;
+        cout<<"Error occured: "<<e<<endl;  
         return 0;
     }
-
-    int res = a/b;
-
-    cout<<"Division of a and b is: "<<res<<endl;
-    return 0;
+    // Output is : 
+    //            Enter two numbers: 6 0
+    //            Error occured: Divide is not possible as denominator is 0
 
 }
 
-// Output is :
-//             Enter two numbers: 5 0
-//             Divide is not possible as divide by 0 is possible
 
-//             Enter two numbers: 6 2
-//             Division of a and b is: 3
